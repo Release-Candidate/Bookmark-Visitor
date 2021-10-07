aba12e93373d14228b2ddfd9c57bdb1e3b4c26d4
Release-Candidate <rec@gmx.at>
2021-10-07 19:58:14 +0200

    Add code

--- Chrome/popup.js
+++ Chrome/popup.js
@@ -9,8 +9,6 @@
 // `chrome` is unknown to eslint
 /* eslint-disable no-undef */
 
-let indentGlobal = 0
-
 const idTreeRoot = "0"
 
 const numIndentSpaces = 4
@@ -21,49 +19,43 @@ let folderDiv = document.getElementById("folders")
 
 let titleElem = document.getElementById("title")
 
-let urlAnchor = document.getElementById("urlA")
-
 document.querySelectorAll("[data-locale]").forEach((elem) => {
     elem.innerText = chrome.i18n.getMessage(elem.dataset.locale)
+})
+
+let urlAnchor = document.getElementById("urlA")
+urlAnchor.addEventListener("click", async () => {
+    runOnCurrentItem(async (currentItem) => {
+        chrome.tabs.create({
+            active: false,
+            url: getURL(currentItem),
+        })
+    })
 })
 
 let nextButton = document.getElementById("nextButton")
 nextButton.addEventListener("click", async () => {
     chrome.storage.local.get("currentItemId", async ({ currentItemId }) => {
         folderDiv.innerText = ""
-        let [currentItem] = await chrome.bookmarks.getSubTree(currentItemId)
-        let [parent] = await chrome.bookmarks.getSubTree(currentItem.parentId)
-        let found = false
-        let itemIdToSave = currentItemId
-        for (child of parent.children) {
-            if (child.index === currentItem.index + 1) {
-                chrome.storage.local.set({ currentItemId: child.id })
-                found = true
-                itemIdToSave = child.id
-                break
-            }
-        }
-        if (!found) {
-            let [grandParent] = await chrome.bookmarks.getSubTree(
-                parent.parentId
-            )
-            let found = false
-            for (child of grandParent.children) {
-                if (child.index === parent.index + 1) {
-                    chrome.storage.local.set({ currentItemId: child.id })
-                    found = true
-                    itemIdToSave = child.id
-                    break
-                }
-            }
-        }
-        if (found) {
-            await mainEntry({ currentItemId: itemIdToSave })
-        }
+
+        await setToNextId(currentItemId)
     })
 })
 
 let deleteButton = document.getElementById("deleteButton")
+deleteButton.addEventListener("click", async () => {
+    runOnCurrentItemId(async (currentItemId) => {
+        if (
+            // eslint-disable-next-line no-alert
+            window.confirm(
+                "Really delete Bookmark '" + titleElem.innerText + "'?"
+            )
+        ) {
+            await setToNextId(currentItemId)
+            await chrome.bookmarks.remove(currentItemId)
+        }
+    })
+})
 
 chrome.storage.local.get("currentItemId", mainEntry)
 
@@ -73,33 +65,129 @@ chrome.storage.local.get("currentItemId", mainEntry)
  *
  * @param {*} currentItemId
  */
-async function mainEntry({ currentItemId }) {
-    let currentId = ""
-    if (typeof currentItemId === "undefined") {
-        currentId = idTreeRoot
-    } else {
-        currentId = currentItemId
-    }
+async function setToNextId(currentItemId) {
+    await getNextID({
+        currentItemId,
+        func: async (nextItemId) => {
+            await mainEntry({ currentItemId: nextItemId })
+        },
+    })
+}
 
-    let currentItem = await getFirstBookmark(currentId)
+/**
+ *
+ * @param {*} param0
+ */
+async function getNextID({ currentItemId, func }) {
+    let [currentItem] = await chrome.bookmarks.getSubTree(currentItemId)
+    let [parent] = await chrome.bookmarks.getSubTree(currentItem.parentId)
+    let found = false
+    let itemIdToSave = currentItemId
+    for (child of parent.children) {
+        if (child.index === currentItem.index + 1) {
+            chrome.storage.local.set({ currentItemId: child.id })
+            found = true
+            itemIdToSave = child.id
+            break
+        }
+    }
+    if (!found) {
+        let [grandParent] = await chrome.bookmarks.getSubTree(parent.parentId)
+        found = false
+        for (child of grandParent.children) {
+            if (child.index === parent.index + 1) {
+                chrome.storage.local.set({ currentItemId: child.id })
+                found = true
+                itemIdToSave = child.id
+                break
+            }
+        }
+    }
+    if (found) {
+        await func(itemIdToSave)
+    }
+}
+
+/**
+ *
+ * @param {*} func
+ */
+async function runOnCurrentItemId(func) {
+    chrome.storage.local.get("currentItemId", async ({ currentItemId }) => {
+        const itemId = await getCheckedId(currentItemId)
+        func(itemId)
+    })
+}
+
+/**
+ *
+ * @param {*} func
+ */
+async function runOnCurrentItem(func) {
+    runOnCurrentItemId(async (currentItemId) => {
+        const itemId = await getCheckedId(currentItemId)
+
+        const currentItem = await getFirstBookmark(itemId)
+        if (currentItem) {
+            await func(currentItem)
+        }
+    })
+}
+
+/**
+ *
+ * @param {*} currentItemId
+ */
+async function mainEntry({ currentItemId }) {
+    const currentItem = await getCheckedId(currentItemId).then(
+        async (currentId) => getFirstBookmark(currentId)
+    )
+
     if (currentItem) {
         chrome.storage.local.set({ currentItemId: currentItem.id })
         await getParents(currentItem)
     }
 }
 
+/**
+ *
+ * @param {*} currentItemId
+ * @returns
+ */
+async function getCheckedId(currentItemId) {
+    if (typeof currentItemId === "undefined") {
+        return idTreeRoot
+    }
+    try {
+        await chrome.bookmarks.get(currentItemId)
+        return currentItemId
+    } catch {
+        return idTreeRoot
+    }
+}
+
+/**
+ *
+ * @param {*} item
+ * @returns
+ */
 async function getParents(item) {
     if (item.id === idTreeRoot) {
         return
     }
-    let [parent] = await chrome.bookmarks.getSubTree(item.parentId)
+    const [parent] = await chrome.bookmarks.getSubTree(item.parentId)
     folderDiv.innerText =
         getTitle(parent) + folderSeparator + folderDiv.innerText
     getParents(parent)
 }
 
+/**
+ *
+ * @param {*} itemId
+ * @returns
+ */
 async function getFirstBookmark(itemId) {
-    let items = await chrome.bookmarks.getSubTree(itemId)
+    const items = await chrome.bookmarks.getSubTree(itemId)
 
     for (item of items) {
         if (item.url) {
@@ -123,10 +211,6 @@ async function getFirstBookmark(itemId) {
  */
 function getIndent(level) {
     return " ".repeat(level * numIndentSpaces)
-}
-
-function getId(item) {
-    return typeof item === "undefined" ? idTreeRoot : item.id
 }
 
 /**
